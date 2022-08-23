d3dcac90001b26823b32f8a87eda021efdf3e949
MateuszPiskorski <48478314+MateuszPiskorski@users.noreply.github.com>
2022-08-23 16:49:32 +0200

    Update settings.py

--- reservation/settings.py
+++ reservation/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u^ik07tf_wby76(x@5)@oshc=5k0oee6+ira+qn1ush&2k_bsr'
+SECRET_KEY = '*******'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
